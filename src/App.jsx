256e817678e287a5bc7af9c433d6111d178de388
arocha2 <abda08.ab@gmail.com>
2024-10-10 22:29:12 -0400

    header img responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,36 @@
+import { useState } from "react";
+import headerImg from "../src/assets/images/background-pattern-desktop.svg";
+import headerMobileImg from "../src/assets/images/background-pattern-mobile.svg";
+import { useEffect } from "react";
+
 export const App = () => {
-  return <div className="text-3xl font-bold underline">App</div>;
+  const [imageSrc, setImageSrc] = useState("");
+
+  const updateImage = () => {
+    const width = window.innerWidth;
+
+    console.log("first");
+
+    if (width < 600) {
+      setImageSrc(headerMobileImg); // Ruta de la imagen para pantallas pequeñas
+    } else {
+      setImageSrc(headerImg); // Ruta de la imagen para pantallas medianas
+    }
+  };
+
+  useEffect(() => {
+    // Actualizar la imagen al cargar el componente
+    updateImage();
+    window.addEventListener("resize", updateImage);
+    return () => {
+      window.removeEventListener("resize", updateImage);
+    };
+  }, []);
+
+  return (
+    <main className="w-full h-[100vh]">
+      <img className="w-full" src={imageSrc} alt="header-img" />
+      App
+    </main>
+  );
 };
