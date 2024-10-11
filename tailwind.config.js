08948d6834dbb501c5e7d02ae2fa638ace8a65ed
arocha2 <abda08.ab@gmail.com>
2024-10-10 21:53:57 -0400

    tailwind config and assets

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: ["workSans"],
+      colors: {
+        "Light-pink": "hsl(275, 100%, 97%)",
+        "Grayish-purple": "hsl(292, 16%, 49%)",
+        "Dark-purple": "hsl(292, 42%, 14%)",
+      },
+    },
   },
   plugins: [],
 };
